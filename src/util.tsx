a7b6f54598c8b2b9d613e2ba466e53b5166574a9
Josh Knowles <joshknowles@gmail.com>
2023-05-08 19:38:40 -0400

    Upgrade to raycast/api 1.50.5

--- src/util.tsx
+++ src/util.tsx
@@ -2,9 +2,7 @@ import { getApplications } from "@raycast/api";
 
 export async function checkReflect() {
   const apps = await getApplications();
-  const reflectInstalled = apps.find(
-    (app) => app.bundleId === "app.reflect.ReflectDesktop"
-  );
+  const reflectInstalled = apps.find((app) => app.bundleId === "app.reflect.ReflectDesktop");
   if (reflectInstalled) return true;
 
   return false;
